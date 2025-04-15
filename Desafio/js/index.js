9cf2c1eb02ff3a6d04dd4fecf4bba04668c655be
JoaoLyra1993 <jmlporto1993@gmail.com>
2025-04-15 11:44:09 -0300

    Ajuste do projeto - 15/04/2025

--- Desafio/js/index.js
+++ Desafio/js/index.js
@@ -1,49 +1,91 @@
-const weatherURL = "http://api.weatherapi.com/v1/current.json?key=3d9c4822c52848ce88b134021250404&q="
-const defaultCity = "Maceió"
+const weatherURL =
+  "http://api.weatherapi.com/v1/current.json?key=3d9c4822c52848ce88b134021250404&q=";
+const newsURL =
+  "https://gnews.io/api/v4/search?q=exemple&lang=en&max=3&apikey=e4082adb340ee17ad39ff1eabf7f1e6a";
+const defaultCity = "Maceió";
 
-function getCity(){
-    if(document.getElementById("searchBar").value != ''){
-        return document.getElementById("searchBar").value
-        
-    }else{
-        return defaultCity
-        }
+function getCity() {
+  if (document.getElementById("searchBar").value != "") {
+    return document.getElementById("searchBar").value;
+  } else {
+    return defaultCity;
+  }
 }
 
-async function callApi(url, city) {
-    const resposta = await fetch(url + city);
-
-    if(resposta.status === 200) {
-        const dados = await resposta.json();
-        //console.log(dados)
-        createElements(dados, city)
-    }else{
-        console.log(resposta.status)
-    }
-    
+async function callApiWeather(url, city) {
+  const resposta = await fetch(url + city);
+  if (resposta.status === 200) {
+    const dados = await resposta.json();
+    createWeather(dados, city);
+    callNewsApi(dados.location.country);
+  } else {
+    console.log(resposta.status);
+  }
 }
 
-function createElements(dados, city){   
+async function callNewsApi(pais) {
+  let urlCompleta = newsURL.replace("exemple", pais);
 
-    const cidade = document.getElementById("h2-clima")
-    const img = document.getElementById("img-status")
-    const temperatura = document.getElementById("temperatura")
-    const umidade = document.getElementById("umidade")
-    const vel_vento = document.getElementById("vel-vento")
+  const resposta = await fetch(urlCompleta);
+  if (resposta.status === 200) {
+    let dados = await resposta.json();
+    createNews(dados);
+    console.log(dados.articles[0]);
+    console.log(dados.articles.length);
+  } else {
+    console.log(resposta.status);
+  }
+}
 
-    cidade.innerHTML =  "Clima em " + city
-    img.src = "https:" + dados.current.condition.icon
-    temperatura.innerText = `Temperatura: ${dados.current.temp_c}° C`
-    umidade.innerText = `Umidade: ${dados.current.humidity} %`
-    vel_vento.innerText = `Velocidade do vento: ${dados.current.wind_kph} km/h`
+function createNews(dados) {
+  for (i = 0; i < dados.articles.length; i++) {
+    if (document.getElementById("div" + i).hasChildNodes() == false) {
+      let titulo = document.createElement("h5");
+      let paragrafo = document.createElement("p");
+      let link = document.createElement("a");
 
-    let texto = document.createElement("input")
-    texto.innerText
+      titulo.setAttribute("id", "titulo" + i);
+      titulo.innerText = dados.articles[i].title;
+
+      paragrafo.setAttribute("id", "p" + i);
+      paragrafo.innerText = dados.articles[i].description;
+
+      link.setAttribute("id", "link" + i);
+      link.setAttribute("href", dados.articles[i].url);
+      link.innerText = "Clique aqui para continuar lendo";
+      link.setAttribute("target", "_blank");
+
+      document.getElementById("div" + i).appendChild(titulo);
+      document.getElementById("div" + i).appendChild(paragrafo);
+      document.getElementById("div" + i).appendChild(link);
+    } else {
+      document.getElementById("titulo" + i).innerText = dados.articles[i].title;
+      document.getElementById("p" + i).innerText =
+        dados.articles[i].description;
+      document
+        .getElementById("link" + i)
+        .setAttribute("href", dados.articles[i].url);
     }
+  }
+}
 
+function createWeather(dados, city) {
+  const cidade = document.getElementById("h3-clima");
+  const img = document.getElementById("img-status");
+  const temperatura = document.getElementById("temperatura");
+  const umidade = document.getElementById("umidade");
+  const vel_vento = document.getElementById("vel-vento");
 
-function run(){
+  cidade.innerHTML = "Clima em " + city;
+  img.src = "https:" + dados.current.condition.icon;
+  temperatura.innerText = `Temperatura: ${dados.current.temp_c}° C`;
+  umidade.innerText = `Umidade: ${dados.current.humidity} %`;
+  vel_vento.innerText = `Velocidade do vento: ${dados.current.wind_kph} km/h`;
 
- callApi(weatherURL, getCity())
+  let texto = document.createElement("input");
+  texto.innerText;
+}
 
+function run() {
+  callApiWeather(weatherURL, getCity());
 }
